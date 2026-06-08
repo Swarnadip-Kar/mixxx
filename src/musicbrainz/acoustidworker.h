f0cdd7eabd0dee4a9464b145ddbd2a70897c3c78
Swarnadip Kar <swarnadip.kr@gmail.com>
2026-06-08 10:31:51 +0530

    Worker: Extend LookupResult with releaseId, artistId, trackId fields

--- src/musicbrainz/acoustidworker.h
+++ src/musicbrainz/acoustidworker.h
@@ -66,6 +66,10 @@ class AcoustIdWorker : public WorkerThread {
         QString acoustidId;        // AcoustID entry UUID (result.id field)
         double score{0.0};         // Confidence score 0.0–1.0
         QList<QUuid> recordingIds; // MusicBrainz recording UUIDs
+
+        QString releaseId; // recordings[0].releases[0].id
+        QString artistId;  // recordings[0].releases[0].artists[0].id
+        QString trackId;   // recordings[0].releases[0].mediums[0].tracks[0].id
     };
 
     /// Returns true if api.acoustid.org responds to a HEAD request within
